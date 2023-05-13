b83fe7665b800162eb0ee86b90deb20aed266b16
ludivineConstanti <ludivine.constanti@outlook.fr>
2023-05-13 14:44:08 +0200

    now can tell if someone should get a gift or not

--- client/index.js
+++ client/index.js
@@ -1,17 +1,25 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+const merkleTree = new MerkleTree(niceList);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Sidney Kertzmann";
+  const index = niceList.indexOf(name);
+  const proof = merkleTree.getProof(index);
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      proof,
+      name,
+    });
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+    console.log({ gift });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 main();
